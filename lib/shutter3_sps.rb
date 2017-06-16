e1fcd6b10e71826b8a725502c45106ca399feb63
Your Name <you@example.com>
2017-06-16 11:20:20 +0100

    1st commit

---
+++ lib/shutter3_sps.rb
@@ -0,0 +1,60 @@
+#!/usr/bin/env ruby
+
+# file: shutter3_sps.rb
+
+require 'sps-pub'
+require 'shutter3'
+
+
+class Shutter3Sps < Shutter3
+
+  def initialize(bdid, topic: 'shutter3', sps_address: nil, sps_port: 59000)
+
+    raise 'Shutter3Sps: Please provide a SPS addres' unless sps_address
+    super(bdid)
+    @sps = SPSPub.new host: sps_address
+    @topic = topic
+  end
+
+  def on_android_keypress()
+    @sps.notice @topic + ': button2 pressed'
+  end
+
+  def on_android_keydown()
+
+  end
+
+  def on_android_keyup()
+
+  end
+
+  def on_connect()
+    @sps.notice @topic + ': connected'
+  end
+
+  def on_disconnect()
+
+    @sps.notice @topic + ': dicconnected'
+  end
+
+  def on_ios_keypress()
+    @sps.notice @topic + ': button1 pressed'
+  end
+
+  def on_ios_keydown()
+
+  end
+
+  def on_ios_keyup()
+
+  end
+
+end
+
+if __FILE__ == $0 then
+
+  br = Shutter3Sps.new bdid=ARGV[0], sps_address: ARGV[1]
+  br.start
+
+end
+
---
+++ shutter3_sps.gemspec
@@ -0,0 +1,15 @@
+Gem::Specification.new do |s|
+  s.name = 'shutter3_sps'
+  s.version = '0.1.0'
+  s.summary = 'Publishes Shutter3 (see the Shutter3 gem) button event ' + 
+      'notifications through the SimplePubSub (SPS) messaging broker.'
+  s.authors = ['James Robertson']
+  s.files = Dir['lib/shutter3_sps.rb']
+  s.add_runtime_dependency('shutter3', '~> 0.1', '>=0.1.3')
+  s.add_runtime_dependency('sps-pub', '~> 0.5', '>=0.5.5')
+  s.signing_key = '../privatekeys/shutter3_sps.pem'
+  s.cert_chain  = ['gem-public_cert.pem']
+  s.license = 'MIT'
+  s.email = 'james@jamesrobertson.eu'
+  s.homepage = 'https://github.com/jrobertson/shutter3_sps'
+end
